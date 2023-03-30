9e247525ce6b346a4aacf787a994f699f0aa66e9
zhuchunlai <myemail_zhu@163.com>
2023-03-31 00:50:14 +0800

    对printErrorLog方法抽离到utils包中

--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -2,7 +2,16 @@
 import log from './log.js';
 import isDebug from './isDebug.js';
 
+function printErrorLog(e, type) {
+    if (isDebug()) {
+        log.error(type, e)
+    } else {
+        log.error(type, e.message)
+    }
+}
+
 export {
     log,
-    isDebug
+    isDebug,
+    printErrorLog
 };
